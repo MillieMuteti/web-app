c15ce8a373746bd9e8191622c8b736f429b87787
Millie Muteti <milliemutetigmail.com>
2019-07-12 09:14:26 -0400

    add centuary month day formula

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 function calculate(){
-    var x = document.getElementById("century").value;
-    alert(x);
+    var CC = document.getElementById("century").value;
+    alert(CC);
+    var YY=document.getElementById("year").value;
+    alert(YY);
+    var MM=document.getElementById("month").value;
+    alert(MM);
+    var DD=document.getElementById("day").value;
+    alert(DD);
 }
 
-/*function day(CC,DD,MM,YY){
-    return(D= ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7)
+function day(CC,DD,MM,YY){
+    return ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7;
 }
-alert(day(CC,MM,DD,YY).toFixed())*/
+function akan(12,23,MM,YY){
+ alert(parseInt(day(CC,DD,MM,YY).toFixed));
+}
 
