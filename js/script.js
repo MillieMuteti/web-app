7255dd8fcb8590d3843d9388cd53eb51550d5145
Millie Muteti <milliemutetigmail.com>
2019-07-12 07:26:07 -0400

    Add forula that links form to js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 function calculate(){
-    
+    var x = document.getElementById("century").value;
+    alert(x);
 }
-function day(CC,DD,MM,YY){
+
+/*function day(CC,DD,MM,YY){
     return(D= ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7)
 }
-alert(day(CC,MM,DD,YY).toFixed())
+alert(day(CC,MM,DD,YY).toFixed())*/
 
