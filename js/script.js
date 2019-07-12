ab52da0b8ea4288caac72d67075056000c3b60e1
Millie Muteti <milliemutetigmail.com>
2019-07-12 04:24:31 -0400

    Make changes in the function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 function day(CC,DD,MM,YY){
-    return(D= ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7)
+    return(D= ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7)
 }
+alert(day(CC,MM,DD,YY).toFixed())
+
